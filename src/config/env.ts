98e38191c46f0616e9e6bf936fb722741f9d5cf6
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2025-04-08 17:05:27 +0200

    Initial commit with project structure

---
+++ src/config/env.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const API_ID = process.env.TG_API_ID!;
+export const API_HASH = process.env.TG_API_HASH!;
+export const MONGO_URI = process.env.MONGO_URI!;
