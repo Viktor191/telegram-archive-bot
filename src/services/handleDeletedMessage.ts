0263297137789e9c4fb5af4c80126b11d207c315
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2025-04-16 15:44:50 +0200

    fix /markMessageAsRemoved

--- src/services/handleDeletedMessage.ts
+++ src/services/handleDeletedMessage.ts
@@ -1,38 +1,33 @@
 import { Api, TelegramClient } from "telegram";
 import { markMessageAsRemoved } from "../utils/markMessageAsRemoved";
-import { saveMessageToFile } from "./saveMessageToFile";
 
 export const handleDeletedMessage = async (client: TelegramClient) => {
     client.addEventHandler((update: any) => {
         // Обработка удаления сообщений в обычных чатах и группах
         if (update instanceof Api.UpdateDeleteMessages) {
             update.messages.forEach((msgId: number) => {
-                const updatedMessage = markMessageAsRemoved(msgId);
-                if (updatedMessage) {
-                    console.log("📨 Удалены сообщения:", updatedMessage);
+                const updatedMessages = markMessageAsRemoved(msgId);
+                if (updatedMessages && updatedMessages.length > 0) {
+                    updatedMessages.forEach((msg: any) => {
+                        console.log("📨 Удалены сообщения:", msg);
+                    });
+                } else {
+                    console.warn(`Сообщения с messageId ${msgId} не найдены.`);
                 }
             });
-            // Можно сохранить событие удаления, если нужно
-            /*saveMessageToFile({
-                type: "deleted",
-                messages: update.messages,
-                timestamp: new Date().toISOString()
-            });*/
         }
 
         // Обработка удаления сообщений в каналах
         if (update instanceof Api.UpdateDeleteChannelMessages) {
             update.messages.forEach((msgId: number) => {
-                const updatedMessage = markMessageAsRemoved(msgId);
-                if (updatedMessage) {
-                    console.log("📨 Удалены сообщения:", updatedMessage);
+                const updatedMessages = markMessageAsRemoved(msgId);
+                if (updatedMessages && updatedMessages.length > 0) {
+                    updatedMessages.forEach((msg: any) => {
+                        console.log("📨 Удалены сообщения:", msg);
+                    });
+                } else {
+                    console.warn(`Сообщения с messageId ${msgId} не найдены.`);
                 }
-            });
-            saveMessageToFile({
-                type: "deleted",
-                channelId: update.channelId.toString(),
-                messages: update.messages,
-                timestamp: new Date().toISOString()
             });
         }
     });
